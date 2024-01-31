92c31f804df03dd8500597491d5407c30d61c049
JosiahFu <josiahfu@gmail.com>
2024-01-30 16:07:10 -0800

    Update item model to be multilayered

--- src/main/java/archives/tater/dogwhistle/datagen/ModelGenerator.java
+++ src/main/java/archives/tater/dogwhistle/datagen/ModelGenerator.java
@@ -3,9 +3,8 @@ package archives.tater.dogwhistle.datagen;
 import archives.tater.dogwhistle.DogWhistle;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.data.client.BlockStateModelGenerator;
-import net.minecraft.data.client.ItemModelGenerator;
-import net.minecraft.data.client.Models;
+import net.minecraft.data.client.*;
+import net.minecraft.util.Identifier;
 
 public class ModelGenerator extends FabricModelProvider {
     public ModelGenerator(FabricDataOutput output) {
@@ -20,6 +19,8 @@ public class ModelGenerator extends FabricModelProvider {
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
         itemModelGenerator.register(DogWhistle.DOG_WHISTLE, Models.GENERATED);
-        DogWhistle.DYED_DOG_WHISTLES.values().forEach(item -> itemModelGenerator.register(item, Models.GENERATED));
+        DogWhistle.DYED_DOG_WHISTLES.values().forEach(item ->
+                Models.GENERATED_TWO_LAYERS.upload(ModelIds.getItemModelId(item), TextureMap.layered(TextureMap.getId(DogWhistle.DOG_WHISTLE), new Identifier(DogWhistle.MOD_ID, TextureMap.getId(item).getPath() + "_overlay")), itemModelGenerator.writer)
+        );
     }
 }
